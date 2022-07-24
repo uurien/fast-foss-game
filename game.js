07c9019eb07492c4abb613a3916d2427dfa6aae8
uurien <ugaitz.urien@gmail.com>
2022-07-24 02:53:54 +0200

    Look and feel

--- game.js
+++ game.js
@@ -1,13 +1,75 @@
 GAME = typeof GAME === 'undefined' ? {} : GAME;
+GAME.running = false;
+GAME.start = function () {
+    if(!GAME.running) {
+        init();
+        setTimeout(() => {
+            document.getElementById('menu').classList.add('hidden')
+        }, 100)
+    }
+}
+GAME.restart = function () {
+    init();
+}
+const MAX_SECONDS = 120;
+let pendingTime
+let timerInterval;
+let timerElement;
+
+function init () {
+    GAME.running = true;
+    pendingTime = MAX_SECONDS;
+    currentPlayer2 = 0
+    viewPlaying();
+    startTimer();
+    drawTime();
+    initPlayers();
+}
+
+function initPlayers () {
+    playerPosition = {x: 0, y: 15}
+    player2Positions = [{x: 15, y: 15}, {x: 13, y: 17}, {x: 17, y: 17}, {x: 15, y: 17}]
+    player.setX(getPixels(playerPosition.x))
+    player.setY(getPixels(playerPosition.y))
+    for (let i = 0; i < PLAYER2_SIZE; i++) {
+        const player2Position = player2Positions[i];
+        player2[i].setX(getPixels(player2Position.x))
+        player2[i].setY(getPixels(player2Position.y))
+    }
+}
+
+function startTimer() {
+    timerElement = document.getElementById('timer')
+    timerInterval = setInterval(updateTimer, 1000)
+}
+function drawTime () {
+    const seconds = pendingTime % 60;
+    const minutes = Math.floor(pendingTime / 60);
+    timerElement.innerText = `${minutes}:${seconds < 10 ? '0': ''}${seconds}`;
+}
+function updateTimer () {
+    if (GAME.running) {
+        pendingTime--;
+        drawTime()
+        if (pendingTime === 0) {
+            GAME.running = false
+            stopTimer();
+            p1Win();
+        }
+    } else {
+        stopTimer()
+    }
+}
+function stopTimer () {
+    clearInterval(timerInterval);
+    timerInterval = null;
+}
 const WALL_CODE = 1;
-const BIG_BALL_CODE = 2;
-const SMALL_BALL_CODE = 0;
-const EMPTY_CODE = 3;
-let playing = true
 const config = {
     type: Phaser.AUTO,
     width: 775,
     height: 775,
+    backgroundColor: '#ffffff',
     physics: {
         default: 'arcade',
         arcade: {}
@@ -20,20 +82,20 @@ const config = {
 };
 let player
 let cursors
-let dKey
 let p2Buttons = {}
 const unit = 25;
 const maxUnitSize = 30;
-const playerPosition = {x: 0, y: 15}
-const player2Positions = [{x: 15, y: 15}, {x: 13, y: 17}, {x: 17, y: 17}, {x: 15, y: 17}]
+let playerPosition = {x: 0, y: 15}
+let player2Positions = [{x: 15, y: 15}, {x: 13, y: 17}, {x: 17, y: 17}, {x: 15, y: 17}]
 const PLAYER2_SIZE = 4; // [1-4]
 const player2 = [];
-let currentPlayer2 = 0;
+let currentPlayer2;
 function getPixels(units) {
     return (unit/2) + (unit * units)
 }
 const game = new Phaser.Game(config);
 let platforms;
+
 function preload ()
 {
     this.load.image('wall', 'assets/wall.png');
@@ -55,26 +117,6 @@ function drawMap(map) {
     }
 }
 
-function drawBigBalls (map, physics) {
-    for (let i = 0; i <= maxUnitSize; i++) {
-        for (let j = 0; j <= maxUnitSize; j++) {
-            if (map[i][j] === BIG_BALL_CODE) {
-                physics.add.sprite(getPixels(j), getPixels(i), 'ball-big')
-            }
-        }
-    }
-}
-
-function drawSmallBalls (map, physics) {
-    for (let i = 0; i <= maxUnitSize; i++) {
-        for (let j = 0; j <= maxUnitSize; j++) {
-            if (map[i][j] === SMALL_BALL_CODE) {
-                physics.add.sprite(getPixels(j), getPixels(i), 'ball-small')
-            }
-        }
-    }
-}
-
 function create ()
 {
     for (let i = 0; i <= 3; i++) {
@@ -85,12 +127,9 @@ function create ()
     platforms = this.physics.add.staticGroup();
     drawMap(GAME.map)
     player = this.physics.add.sprite(getPixels(playerPosition.x), getPixels(playerPosition.y), 'smile');
-    console.log(player)
     for (let i = 0; i < PLAYER2_SIZE; i++) {
         player2.push(this.physics.add.sprite(getPixels(player2Positions[i].x), getPixels(player2Positions[i].y), 'enemy-normal'));
     }
-    // drawBigBalls(GAME.map, this.physics)
-    // drawSmallBalls(GAME.map, this.physics)
     cursors = this.input.keyboard.createCursorKeys()
     p2Buttons.a = this.input.keyboard.addKey('a');
     p2Buttons.s = this.input.keyboard.addKey('s');
@@ -113,6 +152,7 @@ function isValidPosition(x,y) {
     }
     return false
 }
+
 function movePlayer1 () {
     if (!timeoutP1) {
         let newX = playerPosition.x
@@ -137,6 +177,7 @@ function movePlayer1 () {
         }
     }
 }
+
 function movePlayer2 () {
     if (!timeoutP2) {
         const player2Position = player2Positions[currentPlayer2]
@@ -162,19 +203,42 @@ function movePlayer2 () {
         }
     }
 }
+
 function update () {
-    movePlayer1()
-    movePlayer2()
-    for (let i = 0; i < PLAYER2_SIZE; i++) {
-        player2[i].setTexture(i === currentPlayer2 ? 'enemy-selected' : 'enemy-normal')
+    if (GAME.running) {
+        console.log('update')
+        movePlayer1()
+        movePlayer2()
+        for (let i = 0; i < PLAYER2_SIZE; i++) {
+            player2[i].setTexture(i === currentPlayer2 ? 'enemy-selected' : 'enemy-normal')
+        }
     }
 }
 
 function collision () {
-    if (playing) {
-        playing = false
-        alert('P2 win')
-        console.log('collision')
+    if (GAME.running) {
+        GAME.running = false;
+        p2Win();
     }
+}
 
+function viewPlaying () {
+    document.getElementById('menu').classList.add('hidden')
+    document.getElementById('main').classList.add('hidden')
+    document.getElementById('p1-winner').classList.add('hidden')
+    document.getElementById('p2-winner').classList.add('hidden')
+}
+
+function p1Win () {
+    document.getElementById('menu').classList.remove('hidden')
+    document.getElementById('main').classList.add('hidden')
+    document.getElementById('p1-winner').classList.remove('hidden')
+    document.getElementById('p2-winner').classList.add('hidden')
+}
+
+function p2Win () {
+    document.getElementById('menu').classList.remove('hidden')
+    document.getElementById('main').classList.add('hidden')
+    document.getElementById('p1-winner').classList.add('hidden')
+    document.getElementById('p2-winner').classList.remove('hidden')
 }
